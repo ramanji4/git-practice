090e126dfd2882aad2405da41799bb137a4f6877
ramanji4 <ramanji181818@gmail.com>
2025-03-24 21:49:19 +0530

    modified file name

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ram : Hello Karna... How are you..?"
+echo "karna: Hi Ram. I am good. What about you.?"
+echo "Ram : I am also good, how are you doing.?"
+echo "Karna : I am learning shell scripting" 
