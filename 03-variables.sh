181a98f4bff6011745683a0a325e05d5a0c46e06
ramanji4 <ramanji181818@gmail.com>
2025-03-25 21:29:06 +0530

    convo with variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Hemanth
+PERSON2=Manjunath
+
+echo "$PERSON1 : Hello ${PERSON2}... How are you..?"
+echo "${PERSON2}: Hi $PERSON1. I am good. What about you.?"
+echo "$PERSON1 : I am also good, how are you doing ${PERSON2}.?"
+echo "${PERSON2} : I am learning shell scripting $PERSON1."
