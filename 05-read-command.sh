16597598939fd7f8bc4a55fe179be70805feffa7
ramanji4 <ramanji181818@gmail.com>
2025-03-30 14:37:10 +0530

    modifications

--- 05-read-command.sh
+++ 05-read-command.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "Please enter your username and password"
+echo "Please enter your username"
 
 read USERNAME  #Asks input form us by interrupting scipt step by step slow
-                
-read -s PASSWORD #for confidentiality we can use -s to hide the details that we enter
 
 echo "entered username is: $USERNAME"
+
+echo "Please enter your password"
+              
+read -s PASSWORD #for confidentiality we can use -s to hide the details that we enter
+
