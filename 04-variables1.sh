5c302ec0f36fbcbaac8d928f5fc760b6a8ac58b9
ramanji4 <ramanji181818@gmail.com>
2025-03-30 14:05:47 +0530

    modifications

--- 04-variables1.sh
+++ 04-variables1.sh
@@ -3,9 +3,9 @@
 PERSON1=$1
 PERSON2=$2
 PERSON3=$3
-echo"$PERSON1 : Hello $PERSON2, How are you..?"
-echo"$PERSON2 : Hi $PERSON1, I am doing well, what about you..?"
-echo"$PERSON1 : I am also good. I heard that your friend $PERSON3 is learning shell script"
-echo"$PERSON2 : Yes, $PERSON3 is started learning Shell script. 
-echo"$PERSON1 : Okay all the very best to $PERSON3. Bye $PERSON2."
-echo"$PERSON2 : Okay bye $PERSON1."
+echo "$PERSON1 : Hello $PERSON2, How are you..?"
+echo "$PERSON2 : Hi $PERSON1, I am doing well, what about you..?"
+echo "$PERSON1 : I am also good. I heard that your friend $PERSON3 is learning shell script"
+echo "$PERSON2 : Yes, $PERSON3 is started learning Shell script. 
+echo "$PERSON1 : Okay all the very best to $PERSON3. Bye $PERSON2."
+echo "$PERSON2 : Okay bye $PERSON1."
