ad8cbd6da0643c512cf37c46d219de17e32d1acf
ramanji4 <ramanji181818@gmail.com>
2025-03-31 14:30:24 +0530

    conditions

--- 09-package-installations.sh
+++ 09-package-installations.sh
@@ -13,13 +13,15 @@ dnf list installed git
 if [ $? ne 0 ]
 then
     echo "git is not installed... goint to install the git"
+    exit 1
+
     dnf install git -y
     if [ $? ne 0]
     then 
         echo "git installation is not SUCCESS...Please check it"
     else
         echo "git installed SUCCESSFULLY"
-    if
+    fi
 else
     echo "git is already installed...nothing to do"
-if
+fi
