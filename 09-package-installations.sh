54d894cbb294594203fa84530492b665597924da
ramanji4 <ramanji181818@gmail.com>
2025-03-31 14:24:35 +0530

    conditions

---
+++ 09-package-installations.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+then 
+    echo "Please run the script with root previlages"
+    exit 1
+if
+
+dnf list installed git 
+
+if [ $? ne 0 ]
+then
+    echo "git is not installed... goint to install the git"
+    dnf install git -y
+    if [ $? ne 0]
+    then 
+        echo "git installation is not SUCCESS...Please check it"
+    else
+        echo "git installed SUCCESSFULLY"
+    if
+else
+    echo "git is already installed...nothing to do" 
