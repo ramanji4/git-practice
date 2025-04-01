b818bd3646570a3993a63c6d121441b02e7d05aa
ramanji4 <ramanji181818@gmail.com>
2025-04-01 17:12:28 +0530

    functions

---
+++ 11-colors-adding.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=(id -u)
+
+N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+
+ROOT_ACCESS(){
+    if [ $USERDID ne 0 ]
+    then
+        echo "Please run the script with root preveleges"
+        exit 1
+    fi
+}
+
+VALIDATION(){
+    if [ $1 ne 0 ]
+    then 
+        echo "$2 installation is $R FAILED $N "
+    else
+        echo "$2 is installed $G SUCCESSFULLY $N "
+    fi
+}
+
+dnf list installed git 
+
+if [ $? ne 0 ]
+then 
+    echo "git is not installed...going to install" 
+    dnf install git -y 
+    VALIDATION $? "git"
+fi 
+
+
+dnf list installed mysql 
+if [ $? ne 0 ]
+then
+    echo "mysql is not installed...going to install mysql"
+    dnf install mysql -y
+    VALIDATION $? "mySQL"
+fi 
