52b70d122c73114e2a732c2e8f2e3d6e49442b5f
ramanji4 <ramanji181818@gmail.com>
2025-04-05 10:56:52 +0530

    exit status

---
+++ 12-exit-status-with(0).sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+ROOT_ACCESS(){
+    if [ $USERID eq 0 ]
+    then
+        echo "Proceeding with root Priveleges"
+    else
+        echo "PLEASE RUN THE SCRIPT WITH ROOT PRIVELEGES"
+        exit 1
+    fi
+}
