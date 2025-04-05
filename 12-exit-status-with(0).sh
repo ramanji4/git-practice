afa77322a8bbc133af5c5eb8ce33c05d46266c7c
ramanji4 <ramanji181818@gmail.com>
2025-04-05 11:07:49 +0530

    exit status

--- 12-exit-status-with(0).sh
+++ 12-exit-status-with(0).sh
@@ -14,4 +14,14 @@ ROOT_ACCESS(){
         echo "PLEASE RUN THE SCRIPT WITH ROOT PRIVELEGES"
         exit 1
     fi
+} 
+
+VALIDATION(){
+    if [ $? eq 0 ]
+    then
+        echo "$2 is installed already...nothing to do"
+    else
+        echo "$2 is not installed...going to install the $2"
 }
+
+ROOT_ACCESS 
