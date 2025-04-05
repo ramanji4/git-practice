7b8dee5355a9606d29de9c55a9c130de02e371bf
ramanji4 <ramanji181818@gmail.com>
2025-04-05 11:51:47 +0530

    exit status

--- 12-exit-status.sh
+++ 12-exit-status.sh
@@ -22,11 +22,38 @@ VALIDATION(){
         echo "$2 is installed already...nothing to do"
     else
         echo "$2 is not installed...going to install $2"
+    fi 
+}
+
+VALIDATION_2(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 is installed SUCCESSFULLY"
+    else
+        echo "$2 installation id FAILURE...Please check it"
     fi
 }
 
 ROOT_ACCESS 
 
+
+
+
 dnf list installed git 
 
 VALIDATION $? "Git"
+
+dnf install git -y
+
+VALIDATION_2 $? "Git"
+
+
+
+
+dnf list installed nginx
+
+VALIDATION $? "nginx"
+
+dnf install nginx -y
+
+VALIDATION_2 $? "nginx"
