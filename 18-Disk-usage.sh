6d9f30bd45c91535dcdd6730452b7ebabd7bac97
ramanji4 <ramanji181818@gmail.com>
2025-04-11 15:01:57 +0530

    use case

--- 18-Disk-usage.sh
+++ 18-Disk-usage.sh
@@ -3,9 +3,11 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
 
+
+
 while IFS= read -r line
 do
-    USAGE=$(echo $line | grep xfs |awk -F " " '{print $6F}' | cut -d "%" -f1)
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
     DISK_PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
