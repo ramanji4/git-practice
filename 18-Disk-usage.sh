87629698aab5de093ae5d4f5ca53a98d1e7cafb2
ramanji4 <ramanji181818@gmail.com>
2025-04-11 15:23:57 +0530

    use case

--- 18-Disk-usage.sh
+++ 18-Disk-usage.sh
@@ -3,6 +3,9 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
 
+R="\e[31m"
+N="\e[0m"
+
 
 while IFS= read -r line
 do
@@ -10,6 +13,6 @@ do
     DISK_PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "$DISK_PARTITION is more than $DISK_THRESHOLD, Current value: $USAGE. Please check"
+        echo -e "$DISK_PARTITION is more than $DISK_THRESHOLD, Current value: $USAGE. $R Please check $N"
     fi
 done <<< $DISK_USAGE
