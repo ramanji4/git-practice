f08315d4a58ccd031ab9303f57cd87fdb395c8f8
ramanji4 <ramanji181818@gmail.com>
2025-04-11 13:49:19 +0530

    use case

--- 18-Disk-usage.sh
+++ 18-Disk-usage.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=5
 
 while IFS= read -r line
 do
-    USAGE= $(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    USAGE= $(echo $line | awk -F " " '{print $6F}' |cut -d "%" -f1)
     DISK_PARTITION= $(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
