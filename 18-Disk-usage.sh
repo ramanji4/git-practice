e7c4e8b83688da44f1b56259f5eba4f80e54c97c
ramanji4 <ramanji181818@gmail.com>
2025-04-11 12:34:55 +0530

    use case

---
+++ 18-Disk-usage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs )
+DISK_THRESHOLD=5
+
+while IFS= read -r line
+do
+    USAGE= $(echo $line |grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    DISK_PARTITION= $(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $USAGE -ge DISK_THRESHOLD ]
+    then
+        echo "$DISK_PARTITION is more than $DISK_THRESHOLD, Current value is: $DISK_USAGE. Please check it"
+    fi
+done <<< $DISK_USAGE  
