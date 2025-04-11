a3a3c72cbfb6e8959ec236465eccd3ae85d7619b
ramanji4 <ramanji181818@gmail.com>
2025-04-11 16:06:17 +0530

    use case

--- 17-backing-up-files.sh
+++ 17-backing-up-files.sh
@@ -34,24 +34,27 @@ fi
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 echo "Files: $FILES"
 
-if [ ! -z $FILES ] 
+if [ -z $FILES ]
 then
-    echo -e "$G files are found $N"
-    ZIP_FILE="$DESTINATION_DIR/app-logs_$TIME_STAMP.zip"
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+    echo "no files found"
+
+# then
+#     echo -e "$G files are found $N"
+#     ZIP_FILE="$DESTINATION_DIR/app-logs_$TIME_STAMP.zip"
+#     find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
     
-    if [ -f $ZIP_FILE ]
-    then
-        echo -e "$G Successfully Zipped the files $N older than $DAYS"
-        while IFS= read -r file
-        do
-            echo "Deleting the file: $file"
-            rm -rf $file
-        done <<< $FILES
-    else
-        echo -e "Zipping the files is $R FAILED $N"
-        exit 1
-    fi
-else
-    echo -e "No files found older than $DAYS"
-fi
+#     if [ -f $ZIP_FILE ]
+#     then
+#         echo -e "$G Successfully Zipped the files $N older than $DAYS"
+#         while IFS= read -r file
+#         do
+#             echo "Deleting the file: $file"
+#             rm -rf $file
+#         done <<< $FILES
+#     else
+#         echo -e "Zipping the files is $R FAILED $N"
+#         exit 1
+#     fi
+# else
+#     echo -e "No files found older than $DAYS"
+# fi
